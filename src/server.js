4865708d362895dc167c61ca9d3b5a0097dcca3d
Márcio Barbosa <marciobarbosa@indecx.com.br>
2020-03-26 11:18:30 -0300

    commit

--- src/server.js
+++ src/server.js
@@ -3,6 +3,10 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  return res.send(`<h3>Hello World</h3>`);
+});
+
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
